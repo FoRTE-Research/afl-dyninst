be726dc5ca0217de341f6816a72d46b49b6b1f72
Matthew Hicks <mdhicks@gmail.com>
2018-05-04 19:12:36 -0400

    Add error messages and make it match other dyninst libs.

--- libAflDyninst.cpp
+++ libAflDyninst.cpp
@@ -27,49 +27,69 @@ void initAflForkServer() {
   char *shm_env_var = getenv(SHM_ENV_VAR);
 
   if (!shm_env_var) {
-    printf("Error getting shm\n");
+    perror("Error getting shm\n");
+    exit(EXIT_FAILURE);
     return;
   }
+  
   shm_id = atoi(shm_env_var);
+
   trace_bits = (u8 *) shmat(shm_id, NULL, 0);
   if (trace_bits == (u8 *) - 1) {
     perror("shmat");
+    exit(EXIT_FAILURE);
     return;
   }
+  
   // enter fork() server thyme!
   int n = write(FORKSRV_FD + 1, &__afl_temp_data, 4);
 
   if (n != 4) {
-    printf("Error writing fork server\n");
+    perror("Error writing fork server\n");
+    exit(EXIT_FAILURE);
     return;
   }
   while (1) {
     n = read(FORKSRV_FD, &__afl_temp_data, 4);
     if (n != 4) {
       printf("Error reading fork server %x\n", __afl_temp_data);
+      exit(EXIT_FAILURE);
       return;
     }
 
     __afl_fork_pid = fork();
     if (__afl_fork_pid < 0) {
-      printf("Error on fork()\n");
+      perror("Error on fork()\n");
+      exit(EXIT_FAILURE);
       return;
     }
+
+    // Child/worker
     if (__afl_fork_pid == 0) {
       close(FORKSRV_FD);
       close(FORKSRV_FD + 1);
-      break;
-    } else {
-      // parrent stuff
-      n = write(FORKSRV_FD + 1, &__afl_fork_pid, 4);
-      pid_t temp_pid = waitpid(__afl_fork_pid, &__afl_temp_data, 2);
-
-      if (temp_pid == 0) {
-        return;
-      }
-      n = write(FORKSRV_FD + 1, &__afl_temp_data, 4);
+      return; // run benchmarkbreak;
     }
-  }
+    
+    // parrent stuff
+    // Inform controller that we started a new run
+    if (write(FORKSRV_FD + 1, &__afl_fork_pid, 4) != 4) {
+      perror("Fork server write(pid) failed");
+      exit(EXIT_FAILURE);
+    }
+    
+    // Sleep until child/worker finishes
+    if (waitpid(__afl_fork_pid, &__afl_temp_data, 0) < 0) {
+      perror("Fork server waitpid() failed"); 
+      exit(EXIT_FAILURE);
+    }
+    
+    // Inform controller that run finished
+    if (write(FORKSRV_FD + 1, &__afl_temp_data, 4) != 4) {
+      perror("Fork server write(temp_data) failed");
+      exit(EXIT_FAILURE);
+    }
+  } // while(1)
 }
 
 // Should be called on basic block entry
